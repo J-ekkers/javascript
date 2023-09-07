3c7c7c50950a0fd85d0a535b4ed874f747145abb
J-ekkers <143403188+J-ekkers@users.noreply.github.com>
2023-09-07 15:16:44 +0200

    Add files via upload
    javascript if else

---
+++ javascript if else/main.js
@@ -0,0 +1,44 @@
+//opdr 1
+const myNumber = 5.5;
+
+if (myNumber >= 5.4) {
+    alert("je hebt een voldoende")
+}else{
+    alert("je hebt een onvoldoende")
+}
+
+//opdr 2
+myNumber = 5.5;
+
+if (myNumber >= 5.5) {
+    alert("je hebt een voldoende")
+}else{
+    alert("je hebt een onvoldoende")
+}
+
+//opdr 3
+myNumber = 5.5;
+
+if (myNumber = 5) {
+    alert("je moet herkansen")
+}else{
+    alert("je moet niet herkansen")
+}
+
+//opdr 4
+myNumber = 5.5;
+
+if (myNumber = 5.4) {
+    alert("je hebt een voldoende")
+}else{
+    alert("je hebt een onvoldoende")
+}
+
+//opdr 5
+prompt("wat is het wachtwoord?")
+
+if (password = wachtwoord) {
+    alert("u bent ingelogd")
+}else{
+    alert("fout wachtwoord")
+}
