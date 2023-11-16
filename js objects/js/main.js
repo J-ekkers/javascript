8ad9d199e76e88ba31b6bdfcf4fd64fd504d190a
J-ekkers <302790434@student.rocmondriaan.nl>
2023-11-16 12:54:25 +0100

    opdracht

---
+++ js objects/js/main.js
@@ -0,0 +1,79 @@
+console.log("main.js werkt");
+//opdr 1
+let person = {
+  firstName: "donald",
+  lastName: "duck",
+  age: 18,
+  studie: "ICT",
+  score: 200,
+};
+//opdr 2
+let post = {
+  username: "hallo123",
+  title: "gezeik",
+  story: "pc werkt niet",
+  date: "15/11/2023",
+  views: "3.000.000",
+};
+
+//opdr 3
+
+let recipe1 = {
+  title: "pasta",
+  description: "pasta met tomatensaus",
+  healthy: true,
+};
+
+let recipe2 = {
+  title: "kroket",
+  description: "hete ragou met krokant laagje",
+  healthy: false,
+};
+
+let recipe3 = {
+  title: "kip",
+  description: "gefrituurde ki[",
+  healthy: false,
+};
+
+//opdr 4
+
+let bmw1 = {
+  title: "BMW 320 Touring Diesel Xdrive",
+  description:
+    "Nette BMW. Motorisch uitstekend. 184 pk. Interieur lichte gebruikers sporen. Exterieur licht parkeerschade voorbumper. En een schaafplek op de achterklep. Staat op gloednieuwe Continental winterbanden. Extra set velgen ( grotere maat) krijgt u erbij. Km stand 173.000 km. 2 sleutels. Handgreep verlichting. Handelaars mogen verder kijken.",
+  price: 8900.0,
+  kilometer: 173000,
+};
+
+let bmw2 = {
+  title: "BMW 320 Touring Diesel Xdrive",
+  description:
+    "Nette BMW. Motorisch uitstekend. 184 pk. Interieur lichte gebruikers sporen. Exterieur licht parkeerschade voorbumper. En een schaafplek op de achterklep. Staat op gloednieuwe Continental winterbanden. Extra set velgen ( grotere maat) krijgt u erbij. Km stand 173.000 km. 2 sleutels. Handgreep verlichting. Handelaars mogen verder kijken.",
+  price: 8900.0,
+  kilometer: 173000,
+};
+
+let bmw3 = {
+  title: "BMW 320 Touring Diesel Xdrive",
+  description:
+    "Nette BMW. Motorisch uitstekend. 184 pk. Interieur lichte gebruikers sporen. Exterieur licht parkeerschade voorbumper. En een schaafplek op de achterklep. Staat op gloednieuwe Continental winterbanden. Extra set velgen ( grotere maat) krijgt u erbij. Km stand 173.000 km. 2 sleutels. Handgreep verlichting. Handelaars mogen verder kijken.",
+  price: 8900.0,
+  kilometer: 173000,
+};
+
+let bmw4 = {
+  title: "BMW 320 Touring Diesel Xdrive",
+  description:
+    "Nette BMW. Motorisch uitstekend. 184 pk. Interieur lichte gebruikers sporen. Exterieur licht parkeerschade voorbumper. En een schaafplek op de achterklep. Staat op gloednieuwe Continental winterbanden. Extra set velgen ( grotere maat) krijgt u erbij. Km stand 173.000 km. 2 sleutels. Handgreep verlichting. Handelaars mogen verder kijken.",
+  price: 8900.0,
+  kilometer: 173000,
+};
+
+let bmw5 = {
+  title: "BMW 320 Touring Diesel Xdrive",
+  description:
+    "Nette BMW. Motorisch uitstekend. 184 pk. Interieur lichte gebruikers sporen. Exterieur licht parkeerschade voorbumper. En een schaafplek op de achterklep. Staat op gloednieuwe Continental winterbanden. Extra set velgen ( grotere maat) krijgt u erbij. Km stand 173.000 km. 2 sleutels. Handgreep verlichting. Handelaars mogen verder kijken.",
+  price: 8900.0,
+  kilometer: 173000,
+};
