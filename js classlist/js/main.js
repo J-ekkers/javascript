e20e23a3182efe47adaf4bcebbe4b965483467d5
J-ekkers <302790434@student.rocmondriaan.nl>
2023-12-14 11:58:05 +0100

    classlist

---
+++ js classlist/js/main.js
@@ -0,0 +1,10 @@
+function toggleErrorClass() {
+    var boxElement = document.getElementById("myBox");
+    boxElement.classList.toggle("error");
+}
+ 
+function toggleAnimation() {
+    var boxElement = document.getElementById("myBox2");
+    boxElement.classList.toggle("blinker");
+    boxElement.classList.toggle("custom-font");
+}
