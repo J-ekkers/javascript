a9116590a5be819086fc27248dc9cacbf4a0cd94
J-ekkers <302790434@student.rocmondriaan.nl>
2023-12-14 12:48:02 +0100

    Update main.js

--- js classlist/js/main.js
+++ js classlist/js/main.js
@@ -1,10 +1,10 @@
 function toggleErrorClass() {
-    var boxElement = document.getElementById("myBox");
+    let boxElement = document.getElementById("myBox");
     boxElement.classList.toggle("error");
 }
  
 function toggleAnimation() {
-    var boxElement = document.getElementById("myBox2");
+    let boxElement = document.getElementById("myBox2");
     boxElement.classList.toggle("blinker");
     boxElement.classList.toggle("custom-font");
 }
